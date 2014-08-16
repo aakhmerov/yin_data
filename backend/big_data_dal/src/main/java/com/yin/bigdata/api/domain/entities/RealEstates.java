0931f04016c26e94ccf87432b9d2594eb784269b
Askar Akhmerov <aakhmerov@ostdlabs.com>
2014-08-16 11:13:29 +0200

    1. add base entities stubs

---
+++ backend/big_data_dal/src/main/java/com/yin/bigdata/api/domain/entities/RealEstates.java
@@ -0,0 +1,7 @@
+package com.yin.bigdata.api.domain.entities;
+
+/**
+ * Created by aakhmerov on 16/08/14.
+ */
+public class RealEstates {
+}
---
+++ backend/big_data_dal/src/main/java/com/yin/bigdata/api/domain/entities/RealEstatesHist.java
@@ -0,0 +1,42 @@
+package com.yin.bigdata.api.domain.entities;
+
+import java.util.Date;
+
+/**
+ * Created by aakhmerov on 16/08/14.
+ */
+public class RealEstatesHist {
+    private Long OBID;
+    private Date GUELTIG_VON;
+    private Date GUELTIG_BIS;
+    private Date ERSTE_AKTIVIERUNG;
+    private Date LETZTE_AKTIVIERUNG;
+    private String POSTLEITZAHL;
+    private String STRASSE;
+    private Long HAUSNRVON;
+    private Long HAUSNRBIS;
+    private String HAUSNR_GERADEUNGERADE;
+    private String GKZ;
+    private String UEBERSCHRIFT;
+    private String COURTAGE;
+    private Long KAUFPREIS;
+    private String KAUFVERMIETET;
+    private Long MIETEINNAHMENPROMONAT;
+
+    private Long MIETEKALT;
+    private String MIETEKAUTION;
+    private Long MIETEPROQM;
+    private Long MIETEWARM;
+    private Long NEBENKOSTEN;
+    private Long ZIMMERANZAHL;
+    private Long WOHNFLAECHE;
+    private Long NUTZFLAECHE;
+    private Long HAUPTFLAECHE;
+    private Long NEBENFLAECHE;
+    private Long GRUNDSTUECKSFLAECHE;
+    private Long BAUJAHR;
+    private String ETAGE;
+    private String ENERGIEAUSWEIS;
+    private Date LETZTE_DEAKTIVIERUNG;
+
+}
