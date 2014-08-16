1c4c1d45959d23e0c93e3f5b53d613c19f120a77
Askar Akhmerov <aakhmerov@ostdlabs.com>
2014-08-16 15:49:33 +0200

    1. add command to export districts data

--- backend/sql/exports.sql
+++ backend/sql/exports.sql
@@ -1,2 +1,4 @@
 LOAD DATA INFILE 'real_estates_20130801_20140731_berlin_living_rent_hist.dsv' INTO TABLE real_estates_hist COLUMNS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n';
-LOAD DATA INFILE 'real_estates_20130801_20140731_berlin_living_rent.dsv' INTO TABLE real_estates COLUMNS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n';
+LOAD DATA INFILE 'real_estates_20130801_20140731_berlin_living_rent_hist.dsv' INTO TABLE real_estates_hist COLUMNS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n';
+-- sudo cp Berlin_Friedrichshain_KBerg_Mitte_apartments_buy.csv /var/lib/mysql/yin
+LOAD DATA INFILE 'Berlin_Friedrichshain_KBerg_Mitte_apartments_buy.csv' INTO TABLE districts_data COLUMNS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' ignore 1 lines;
